5632a8b0d4fa15989494a40155b0f27f7402dfbb
ericzhang <ericzhang@localhost>
2010-05-04 19:40:41 -0400

    Disable moving the excerpt box until we can fix the excerpt tinymce editor creation code

--- plugin/living_stories.js
+++ plugin/living_stories.js
@@ -5,7 +5,14 @@
 var LivingStoryPlugin = {};
 
 LivingStoryPlugin.load = function() {
-
+  // TODO: this breaks the editor, so it's commented out for now.
+  // The problem is: we aren't always able to do this before the
+  // tinyMCE excerpt editor is created.  As a result, when we
+  // move the node, it loses all the event handlers and the
+  // editor becomes dead.  If we call tme_convertExcerpt() a
+  // second time, it screws up the editor and makes it so that it doesn't
+  // save text properly :(
+  /*
   var postexcerpt = document.getElementById('postexcerpt');
   var postdiv = document.getElementById('postdivrich');
   if (postexcerpt && postdiv) {
@@ -13,6 +20,7 @@ LivingStoryPlugin.load = function() {
     postdiv.parentNode.insertBefore(postexcerpt, postdiv);
     tme_convertExcerpt();
   }
+  */
 }
 
-jQuery(document).ready(LivingStoryPlugin.load);
+// jQuery(document).ready(LivingStoryPlugin.load);
